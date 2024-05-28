ba81b7a3d00d241f7f7e00f94451a6a6752892a5
Nirvik Purkait <nirvikpurkait@gmail.com>
2024-05-28 20:20:16 +0530

    styled no details page

--- src/components/no-details/index.tsx
+++ src/components/no-details/index.tsx
@@ -1,15 +1,31 @@
+import { cn } from "@/lib/shadcn-ui/utils";
 import React from "react";
 
 export default function NoDetails() {
   return (
-    <div>
-      <h2>Uh-uh...........</h2>
-      <h3>No details found for the given id</h3>
-      <div>This (may) happend because:</div>
-      <ul>
-        <li>Given id belongs to admin</li>
-        <li>No user is registered with this id</li>
-      </ul>
+    <div
+      className={cn(
+        `mt-10 flex flex-col items-center justify-center space-y-8`
+      )}
+    >
+      <h2
+        className={cn(
+          `mb-6 text-center text-8xl text-blue-500 dark:text-white`
+        )}
+      >
+        Uh-uh...........
+      </h2>
+      <h3 className={cn(`text-center text-4xl`)}>
+        No details found for the given id
+      </h3>
+      <div className={cn(`text-2xl`)}>
+        <div>This (may) happend because:</div>
+        <ul className={cn(`list-disc`)}>
+          <li className={cn(`ml-8`)}>Given id belongs to admin.</li>
+          <li className={cn(`ml-8`)}>No user is registered with this id.</li>
+        </ul>
+      </div>
+      <div className={cn(`text-2xl`)}>Try searching for a valid user-id</div>
     </div>
   );
 }
