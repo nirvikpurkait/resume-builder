2a50b8732346c68df23d928ed1d6dc08118d77f4
Nirvik Purkait <nirvikpurkait@gmail.com>
2024-06-08 02:08:36 +0530

    Merge branch 'docs'

--- src/lib/shadcn-ui/utils.ts
+++ src/lib/shadcn-ui/utils.ts
@@ -1,6 +1,10 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/**
+ * function to [merge](https://www.npmjs.com/package/tailwind-merge) different [types](https://www.npmjs.com/package/clsx) of class values to use
+ * with `className` prop.
+ */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
